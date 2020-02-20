602f3a9a8dafd67e9bf2ac3a68f24ceaa096bddb
Rafael Almeida <rafaelcpalmeida@gmail.com>
2020-02-20 14:15:11 +0000

    Refactor code structure and logic

--- main.go
+++ main.go
@@ -27,10 +27,37 @@ func main() {
 	var emailConfig EmailConfig
 	var emailVariables EmailVariables
 
+	setupVars(&emailConfig, &emailVariables)
+
+	auth := configEmailAuth(emailConfig)
+
+	for k := range emailVariables.Variables {
+		vars := configureEmailVariables(emailConfig, k, emailVariables)
+
+		emailTemplate := configureEmailTemplate(vars)
+
+		fmt.Println("Sending email to: " + vars["RecipientEmail"].(string) + "...")
+		if err := smtp.SendMail(fmt.Sprintf("%s:%s", emailConfig.Host, emailConfig.Port), auth, emailConfig.User, []string{vars["RecipientEmail"].(string)}, []byte(emailTemplate)); err != nil {
+			printErrorAndDie(err)
+		}
+
+		fmt.Println("Email sent!")
+		fmt.Println()
+
+		vars = nil
+	}
+}
+
+func printErrorAndDie(err error) {
+	fmt.Println(err)
+	os.Exit(1)
+}
+
+func setupVars(emailConfig *EmailConfig, emailVariables *EmailVariables) {
 	jsonFile, err := os.Open("email-config.json")
 
 	if err != nil {
-		fmt.Println(err)
+		printErrorAndDie(err)
 	}
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
@@ -39,52 +66,49 @@ func main() {
 	jsonFile, err = os.Open("email-data.json")
 
 	if err != nil {
-		fmt.Println(err)
+		printErrorAndDie(err)
 	}
 
 	byteValue, _ = ioutil.ReadAll(jsonFile)
 	json.Unmarshal(byteValue, &emailVariables)
+}
 
-	auth := smtp.PlainAuth("", emailConfig.User, emailConfig.Password, emailConfig.Host)
+func configEmailAuth(emailConfig EmailConfig) smtp.Auth {
+	return smtp.PlainAuth("", emailConfig.User, emailConfig.Password, emailConfig.Host)
+}
 
-	for k := range emailVariables.Variables {
+func configureEmailVariables(emailConfig EmailConfig, k int, emailVariables EmailVariables) map[string]interface{} {
+	vars := make(map[string]interface{})
 
-		vars := make(map[string]interface{})
-		vars["SenderName"] = emailConfig.SenderName
-		vars["SenderEmail"] = emailConfig.SenderEmail
+	vars["SenderName"] = emailConfig.SenderName
+	vars["SenderEmail"] = emailConfig.SenderEmail
 
-		for _k := range emailVariables.Variables[k] {
-			if str, ok := emailVariables.Variables[k][_k].(string); ok {
-				vars[_k] = str
-			}
+	for _k := range emailVariables.Variables[k] {
+		if str, ok := emailVariables.Variables[k][_k].(string); ok {
+			vars[_k] = str
 		}
-
-		emailTemplate, err := template.New("email.tmpl").Funcs(template.FuncMap{
-			"emailAddressStructure": func(str string) template.HTML {
-				return template.HTML(fmt.Sprintf("<%s>", str))
-			},
-		}).ParseFiles("email.tmpl")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		var emailBytes bytes.Buffer
-		err = emailTemplate.Execute(&emailBytes, vars)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("Sending email to: " + vars["RecipientEmail"].(string) + "...")
-		if err := smtp.SendMail(fmt.Sprintf("%s:%s", emailConfig.Host, emailConfig.Port), auth, emailConfig.User, []string{vars["RecipientEmail"].(string)}, []byte(emailBytes.String())); err != nil {
-			fmt.Println("Error SendMail: ", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("Email sent!")
-		fmt.Println()
-
-		vars = nil
 	}
+
+	return vars
+}
+
+func configureEmailTemplate(vars map[string]interface{}) string {
+	template, err := template.New("email.tmpl").Funcs(template.FuncMap{
+		"emailAddressStructure": func(str string) template.HTML {
+			return template.HTML(fmt.Sprintf("<%s>", str))
+		},
+	}).ParseFiles("email.tmpl")
+
+	if err != nil {
+		printErrorAndDie(err)
+	}
+
+	var emailBytes bytes.Buffer
+	err = template.Execute(&emailBytes, vars)
+
+	if err != nil {
+		printErrorAndDie(err)
+	}
+
+	return emailBytes.String()
 }
