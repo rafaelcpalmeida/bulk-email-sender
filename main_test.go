bef3e15bfb9bd3133a2c9d6d7656149c65e9d9f6
Rafael Almeida <rafaelcpalmeida@gmail.com>
2020-03-10 19:29:55 +0000

    Add tests for openAndReadFile, initEmailConfig and initEmailVariables functions

--- main_test.go
+++ main_test.go
@@ -1,8 +1,44 @@
 package main
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 )
+
+func TestOpenAndReadFile(t *testing.T) {
+	validFileName := "email-config.example.json"
+	invalidFileName := "email-config.example.jsn"
+
+	fileContents := []byte(`{
+    "user": "someone@address.com",
+    "password": "changeMe",
+    "host": "mail.host.com",
+    "port": "587",
+    "sender-name": "Email Sender",
+    "sender-email": "sender@address.com"
+}`)
+
+	validFile, err := openAndReadFile(validFileName)
+
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	strippedFileContents := string(strings.Replace(string(validFile), "\t", "", -1))
+
+	if string(fileContents) == strippedFileContents {
+		t.Logf("openAndReadFile is successful with valid file")
+	} else {
+		t.Errorf("openAndReadFile is unsuccessful, expected %v, got %v", string(fileContents), strippedFileContents)
+	}
+
+	_, err = openAndReadFile(invalidFileName)
+
+	if err != nil {
+		t.Logf("openAndReadFile fails with inexisting file")
+	}
+}
 
 func TestInitEmailConfig(t *testing.T) {
 	validEmailConfigs := EmailConfig{
@@ -15,19 +51,63 @@ func TestInitEmailConfig(t *testing.T) {
 	}
 	invalidEmailConfigs := EmailConfig{}
 
-	validFile, err := initEmailConfig("email-config.example.json")
+	validFileName := "email-config.example.json"
+	validFile, err := openAndReadFile(validFileName)
 
-	if validFile == validEmailConfigs && err == nil {
-		t.Logf("initEmailConfig config is successful with valid file")
-	} else {
-		t.Errorf("initEmailConfig config is unsuccessful, expected %v, got %v", validEmailConfigs, validFile)
+	if err != nil {
+		t.Fatalf("Error: %v", err)
 	}
 
-	inexistingFile, err := initEmailConfig("wrong-email-config.example.json")
+	validFileData, err := initEmailConfig(validFile)
 
-	if inexistingFile == invalidEmailConfigs && err != nil && err.Error() == "open wrong-email-config.example.json: no such file or directory" {
-		t.Logf("initEmailConfig config fails with inexisting file")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	if validFileData == validEmailConfigs {
+		t.Logf("initEmailConfig is successful with valid file")
 	} else {
-		t.Errorf("initEmailConfig config is unsuccessful, expected %v, got %v", validEmailConfigs, inexistingFile)
+		t.Errorf("initEmailConfig is unsuccessful, expected %v, got %v", validEmailConfigs, validFileData)
+	}
+
+	inexistingFile, err := initEmailConfig(make([]byte, 0))
+
+	if inexistingFile == invalidEmailConfigs && err != nil && err.Error() == "unexpected end of JSON input" {
+		t.Logf("initEmailConfig fails with inexisting file")
+	} else {
+		t.Errorf("initEmailConfig is unsuccessful, expected %v, got %v", invalidEmailConfigs, inexistingFile)
+	}
+}
+func TestInitEmailVariables(t *testing.T) {
+	validEmailVariables := EmailVariables{
+		Variables: []map[string]interface{}{0: {"RecipientName": "Email Receiver", "RecipientEmail": "receiver-1@another-address.com", "Variable1": "Value1", "Variable2": "Value2"}, 1: {"RecipientName": "Email Receiver", "RecipientEmail": "receiver-2@another-address.com", "Variable1": "Value1", "Variable2": "Value2"}, 2: {"RecipientName": "Email Receiver", "RecipientEmail": "receiver-3@another-address.com", "Variable1": "Value1", "Variable2": "Value2"}},
+	}
+	invalidEmailVariables := EmailVariables{}
+
+	validFileName := "email-data.example.json"
+	validFile, err := openAndReadFile(validFileName)
+
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	validFileData, err := initEmailVariables(validFile)
+
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	if reflect.DeepEqual(validFileData, validEmailVariables) {
+		t.Logf("initEmailVariables is successful with valid file")
+	} else {
+		t.Errorf("initEmailVariables is unsuccessful, expected %v, got %v", validEmailVariables, validFileData)
+	}
+
+	inexistingFile, err := initEmailVariables(make([]byte, 0))
+
+	if reflect.DeepEqual(inexistingFile, invalidEmailVariables) && err != nil && err.Error() == "unexpected end of JSON input" {
+		t.Logf("initEmailVariables fails with inexisting file")
+	} else {
+		t.Errorf("initEmailVariables is unsuccessful, expected %v, got %v", invalidEmailVariables, inexistingFile)
 	}
 }
